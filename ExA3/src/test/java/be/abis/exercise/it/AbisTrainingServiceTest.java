6649289ad5f1ca5d95d94b8e4d737d83bca439ab
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-10-08 16:29:25 +0200

    Add EXA3

--- ExA3/src/test/java/be/abis/exercise/it/AbisTrainingServiceTest.java
+++ ExA3/src/test/java/be/abis/exercise/it/AbisTrainingServiceTest.java
@@ -1,6 +1,6 @@
 package be.abis.exercise.it;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import be.abis.exercise.model.Person;
+import be.abis.exercise.model.Course;
 import be.abis.exercise.services.CourseService;
 import be.abis.exercise.services.TrainingService;
 
@@ -20,9 +21,19 @@ public class AbisTrainingServiceTest {
 	@Autowired
 	TrainingService trainingService;
 	
+	@Autowired
+	CourseService courseService;
+	
 	@Test
-	public void person1isJohn() {
+	public void person1IsJohn() {
 		Person p1 = trainingService.findPerson(1);
 		assertEquals("John",p1.getFirstName());
+	}
+	
+	@Test
+	public void getTitleCourse8050() {
+		Course c1 = courseService.findCourse(8050);
+		assertEquals("Maven",c1.getShortTitle());
+
 	}
 }
