09135ff7f096adb687ad52f1f41ce995087be27f
KristofDhondt <kristof.dhondt@bnpparibasfortis.com>
2020-10-13 16:43:09 +0200

    EXB3 with issue on search

--- EXB3/src/main/java/be/abis/exercise/controller/AppController.java
+++ EXB3/src/main/java/be/abis/exercise/controller/AppController.java
@@ -2,6 +2,11 @@ package be.abis.exercise.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,10 @@ public class AppController {
 	TrainingService trainingService;
 	
 	Person loggedInUser;
+	Person removedPerson;
+	Person addedPerson;
+			
+	List<Person> listPersons;
 	
 	@GetMapping("/")
 	public String login(Model model) {
@@ -37,25 +46,85 @@ public class AppController {
 		return "welcome";
 	}
 	
-	
-	@PostMapping("/personadmin")
-	public String personadmin (Model model, Login loginUser) {
-		return "redirect:/changepassword";
-	} 
 	@GetMapping("/personadmin")
-	public String personadminscreen(Model model){
+	public String personAdminScreen(Model model){
 		return "personadmin";
 	}
 	
+	@GetMapping("/changepassword")
+	public String changePasswordScreen(Model model){
+		model.addAttribute("person",loggedInUser);
+		return "changepassword";
+	}
+	
+	@GetMapping("/searchperson")
+	public String searchpersonsScreen(Model model){
+		model.addAttribute("person", new Person());
+		return "searchperson";
+	}
+	
+	@GetMapping("/addperson")
+	public String addPersonScreen(Model model){
+		model.addAttribute("person", new Person());
+		return "addperson";
+	}
+	
+	@GetMapping("/removeperson")
+	public String removePersonScreen(Model model){
+		model.addAttribute("person", new Person());
+		return "removeperson";
+	}
+	
+	@GetMapping("/showpersons")
+	public String showPersons(Model model){
+		model.addAttribute("persons", listPersons);
+		return "showpersons";
+	}
+	
+	@GetMapping("/personList")
+	public String personList(){
+		listPersons = trainingService.getAllPersons();	
+		return "redirect:/showpersons";
+	}
 	
 	@GetMapping("/logout")
 	public String logout(){
 		return "redirect:/";
 	}
-	
-	@GetMapping("/back")
-	public String back(){
+		
+	@PostMapping("/changepassword")
+	public String postChangePassword(Model model, Person person) {
+		try {
+			trainingService.changePassword(loggedInUser, person.getPassword());			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return "redirect:/";
 	}
 	
+	@PostMapping("/removePersonById")
+	public String removePersonById(Person person){
+		removedPerson=trainingService.findPerson(person.getPersonId());
+		trainingService.deletePerson(person.getPersonId());	
+		return "redirect:/personadmin";
+	}
+	
+	@PostMapping("/addPerson")
+	public String addPerson(Person person){	
+		try {
+			addedPerson = trainingService.findPerson(person.getPersonId());
+			trainingService.addPerson(person);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		return "redirect:/personadmin";
+	}
+		
+	@PostMapping("/findPersonById")
+	public String findPersonById(Person person){
+		Person personById = trainingService.findPerson(person.getPersonId());	
+		listPersons = new ArrayList<Person>();
+		listPersons.add(personById);
+		return "redirect:/showpersons";
+	}
 }
